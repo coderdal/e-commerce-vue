6493f11a100b0259068e72fbb5eb26250af96a63
coderdal <agircoder@gmail.com>
2022-07-16 12:11:17 +0300

    Vuex Setup & Axios Installation

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,17 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    basket: [],
+    isLoading: true,
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    getWholeProducts() {},
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});
